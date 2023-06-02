99c4866208859d0a106b132c86dbee2f03d401d4
tiendunggithub <tiendung1001gn@gmail.com>
2023-06-02 09:00:46 +0700

    first commit

---
+++ serviceWorker.js
@@ -0,0 +1,24 @@
+const nvtdung = 'nvt_dung'
+const assets = [
+    "/",
+    "home.html",
+    "/css/style.css",
+    "/js/app.js",
+    "/images/sniper.jpg"
+]
+
+self.addEventListener('install', installEvent => {
+    installEvent.waitUntil(
+        caches.open(nvtdung).then(cache => {
+            cache.addAll(assets)
+        })
+    )
+});
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+        caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request);
+        })
+    );
+});
